380f2d74afff5a153b1e6a85a90a344a17c26c60
Mohamad Omar Altayara <omartayara44@gmail.com>
2020-09-05 01:20:50 +0300

    project done

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,9 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 import NotFound from "./components/not-found/NotFound.js";
+import Post from "./components/post/Post.js";
 
 import "./App.css";
 
@@ -87,6 +89,12 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/post/:id" component={Post} />
               </Switch>
               <Route exact path="/not-found" component={NotFound} />
             </div>
